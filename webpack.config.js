d19efb6b0c7e004ede431f0fa3c9d9aef18d626e
Cheng Kuan <cheng.kuan2000@gmail.com>
2020-07-16 11:27:56 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.join(__dirname, 'demo/src/index.js'),
+  entry: path.join(__dirname, 'demo/js/index.js'),
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
